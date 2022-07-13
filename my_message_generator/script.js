408ae71aca54086bc79cb8b50408e498857ef813
Melania <state_melania5@yahoo.com>
2022-07-13 16:15:35 +0100

    Create a different array for each of the three quote categories

---
+++ my_message_generator/script.js
@@ -0,0 +1,8 @@
+//array of inspirational quotes
+const inspirationalQuotes = ["Start by doing what's necessary, then do what's possible; and suddenly you are doing the impossible.", "Every situation in life is temporary.", "Every day may not be good, but there is something good in every day."];
+ 
+//array of motivational quotes
+const motivationalQuotes = ["Be the change that you wish to see in the world.", "It's never too late for a new beginning in your life.", "Stay away from those people who try to disparage your ambitions. Small minds will always do that, but great minds will give you a feeling that you can become great too."];
+ 
+//array of manifesting quotes
+const manifestingQuotes = ["Ask for what you want and be prepared to get it.", "What you think, you create. What you feel, you attract. What you imagine, you become.", "Thoughts become things. If you see it in your mind, you will hold it in your hand."];
